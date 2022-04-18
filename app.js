4a0d7c895134040e9a5f1cb16dc082628f4b4993
Jared <jkpeople1856@gmail.com>
2022-04-18 11:59:57 -0500

    finished exercise

--- app.js
+++ app.js
@@ -5,63 +5,57 @@ console.log(
 
 // Exercise 1
 console.log('Add: ${add}, Minus: ${minus}, Multiply: ${multiply}, Divide: ${divide}, Exp: ${exp}, Mod: ${mod})'
-let x = 20;
-let y = 4;
-let add = x + y;
-let minus = x - y;
-let multiply = x * y;
-let divide = x / y;
-let exp = x ** y;
-let mod = x % y;
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
-
 let a = 11;
 let str = "11";
 let str2 = "eleven";
 let isPresent = true;
-let firstName = "Jackie";
-let lastName = "Chan";
+let firstName = "Jackie"; 
+let lastName = "Chan"; 
 
 // What is the value of: a + str?
-console.log(a + str); // "1111"
+// 1111
 // What is the value of: a + str2?
-console.log(a + str2); // "11eleven"
+// 11eleven 
 // What is the value of: a + isPresent?
-console.log(a + isPresent); // "12"
+// 12 
 // What is the value of: a + firstName?
-console.log(a + firstName); // "11Jackie"
-// What is the value of: a + lastName?
-console.log(isPresent + lastName); // "trueChan"
-
+// 11Jackie 
+// What is the value of: a + lastName? 
+// 11Chan 
 
 // Exercise 3
-console.log("EXERCISE 3:\n==========\n");
-
+console.log("EXERCISE 3:\n==========\n"); 
 let d = 5;
 let str3 = "5";
 let str4 = "five";
 let isPresent2 = false;
 let firstName2 = "Robin";
-let lastName3 = "Williams";
+let lastName2 = "Williams"; 
 
 // What is the value of: d == str3?
-console.log(d == str3); // true
+console.log(d == str3)  // true
 // What is the value of: d === str3?
-console.log(d === str3); // false
-// What is the value of: !isPresent?
-console.log(!d); // false
-// What is the value of: (“eleven” == str2 && a str)?
-console.log("eleven" == str4 && d >= str3); // false
-// What is the value of: (!isPresent || Present)?
-console.log(!isPresent2 || isPresent2); // true
+console.log(d === str3) // false 
+// What is the value of: !isPresent2?
+console.log(!isPresent2) // true
+// What is the value of: ("eleven" == str4 && d >= str3)?
+console.log("eleven" == str4 && d >= str3) // false 
+// What is the value of: (!isPresent2 || isPresent2)?
+console.log(!isPresent2 || isPresent2) // true 
 // What is the value of: 0 == false?
-console.log(0 == false); // true 
+console.log(0 == false) // true
 // What is the value of: 0 === false?
-console.log(0 === false); // false 
+console.log(0 === false) // false 
 // What is the value of: 0 != false?
-console.log(0 != false); // false
-// What is the value of 0 !== false?
-console.log(0 !== false); //true 
-
+console.log(0 != false) // false
+// What is the value of: 0 !== false? 
+console.log(0 !== false) // true 
